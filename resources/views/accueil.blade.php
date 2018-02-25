d8816f9d433378b069f3442b9cdecb0b3e8523b4
Maxime <maxime.dossantos01@gmail.com>
2018-02-25 20:37:36 +0100

    maj comité

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 
-    <div class="page">
+    <div class="page page__accueil">
         <section>
             <div class="page__fond">
                 <img class="page__fond--image" src="images/fond.jpg">
@@ -13,11 +13,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                        <div class="page__entete__texte">
-                            <h1>
+                        <div class="page__accueil__entete">
+                            <img class="page__accueil__entete--icone hidden-xs" src="images/icone.png">
+                            <h1 class="hidden-xs">
                                 <p>Comité</p>
-                                <p>Des</p>
-                                <p>Fêtes</p>
+                                <p class="typo__texte__indent--60">Des</p>
+                                <p class="typo__texte__indent--120">Fêtes</p>
+                                <span>La Verrie</span>
                             </h1>
                         </div>
                     </div>
